7f08a1b1316c077d34f22c5fbfef760c27f247fc
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-10-31 13:50:29 +0530

    Update election.sol

--- election.sol
+++ election.sol
@@ -1,10 +1,29 @@
 pragma solidity >=0.4.0 <0.7.0;
 
 contract Election {
-    string public candidateName = "Candidate Name";
 
-    function setCandidate(string memory _name) public {
-        candidateName = _name;
+    struct candidate {
+        string name;
+        uint age;
+        uint height;
+    }
+    
+    mapping(address => candidate) public candidates;
+    
+
+    function setCandidate(string memory name, uint age, uint height) public {
+        address creator = msg.sender;
+        
+        candidate memory newCandidate;
+        newCandidate.name = name;
+        newCandidate.age = age;
+        newCandidate.height = height;
+        candidates[creator] = newCandidate;
+    }
+    
+    function getCandidate() public view returns (string memory name, uint age, uint height) {
+        address creator = msg.sender;
+        return (candidates[creator].name, candidates[creator].age, candidates[creator].height);
     }
 
     
