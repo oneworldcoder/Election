c1cd208d2d7968cc89dd6a5505f56d369763ffd7
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-11-04 10:18:23 +0530

    Update election.sol

--- election.sol
+++ election.sol
@@ -1,29 +1,41 @@
 pragma solidity >=0.4.0 <0.7.0;
+pragma experimental ABIEncoderV2;
+
 
 contract Election {
 
     struct candidate {
         string name;
+        string politicalParty;
+        string state;
         uint age;
         uint height;
     }
     
+    candidate[] public candidateList;
+    
     mapping(address => candidate) public candidates;
     
 
-    function setCandidate(string memory name, uint age, uint height) public {
+    function setCandidate(string memory name, string memory politicalParty, string memory state, uint age, uint height) public {
         address creator = msg.sender;
         
         candidate memory newCandidate;
         newCandidate.name = name;
+        newCandidate.politicalParty = politicalParty;
+        newCandidate.state = state;
         newCandidate.age = age;
         newCandidate.height = height;
         candidates[creator] = newCandidate;
     }
     
-    function getCandidate() public view returns (string memory name, uint age, uint height) {
+    function getCandidate() public view returns (string memory name, string memory politicalParty, string memory state, uint age, uint height) {
         address creator = msg.sender;
-        return (candidates[creator].name, candidates[creator].age, candidates[creator].height);
+        return (candidates[creator].name, candidates[creator].politicalParty, candidates[creator].state, candidates[creator].age, candidates[creator].height);
+    }
+    
+    function getCandidateList() public view returns (candidate [] memory) {
+        return candidateList;
     }
 
     
